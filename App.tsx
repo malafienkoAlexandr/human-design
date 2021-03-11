734628b0c50b336a7e70fe0dcca0c3e989ea88b8
Malafienko <malafienko@gmail.com>
2021-03-11 17:46:44 +0300

    i18 integrate

--- App.tsx
+++ App.tsx
@@ -4,22 +4,61 @@ import AppLoading from "expo-app-loading";
 import store from "./src/store/index";
 import { bootstrap } from "./src/bootstrap";
 import { AppNavigator } from "./src/navigation/AppNavigation";
+import { View, Text, NativeModules, NativeModulesStatic, Platform } from "react-native";
+import i18next from "i18next";
+import { initReactI18next, useTranslation } from "react-i18next";
+import homeEn from './src/i18n/en/home.json'
+import homeRu from './src/i18n/ru/home.json'
+
+const { RNI18n } = NativeModules;
+
+const locale =
+  Platform.OS === 'ios'
+    ? NativeModules.SettingsManager.settings.AppleLocale
+    : NativeModules.I18nManager.localeIdentifier;
+
+
+const resources = {
+  en: {
+    home: homeEn
+  },
+  ru: {
+    home: homeRu
+  }
+};
+
+i18next
+  .use(initReactI18next)
+  .init({
+    lng: locale.substring(0, 2),
+    fallbackLng: 'ru',
+    debug: false,
+    resources: resources
+  });
+
 
 export default function App() {
   const [isRedy, setIsRedy] = useState(false);
+  const { t } = useTranslation()
 
   if (!isRedy) {
     return (
       <AppLoading
-        startAsync={bootstrap}
+        startAsync={(bootstrap)}
         onFinish={() => setIsRedy(true)}
         onError={(error) => console.log(error)}
       />
     );
   }
+
   return (
-    <Provider store={store}>
-      <AppNavigator />
-    </Provider>
+
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={{ fontSize: 40, marginBottom: 20 }}>
+        {t('hello')}
+      </Text>
+    </View>
+
   );
 }
+
