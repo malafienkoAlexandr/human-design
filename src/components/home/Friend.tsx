3a872525eb40eb867e45d8a542f331425395de66
Alexandr <malafienko@gmail.com>
2021-03-26 14:00:05 +0300

    1

--- src/components/home/Friend.tsx
+++ src/components/home/Friend.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
 import { PersonalForecast } from "PersonalForecast";
 import { User } from "UserModule";
+import { THEME } from "../../theme";
 
 type Props = {
   item: User;
@@ -12,7 +13,7 @@ export const Friend = (props: Props) => {
   return (
     <TouchableOpacity onPress={props.onPress}>
       <View style={styles.main}>
-        <Text>{props.item.name}</Text>
+        <Text style={styles.text}>{props.item.name}</Text>
       </View>
     </TouchableOpacity>
   );
@@ -22,12 +23,18 @@ const styles = StyleSheet.create({
   main: {
     width: 90,
     height: 120,
-    alignSelf: "flex-start",
+
     alignItems: "center",
+    justifyContent: "flex-end",
     backgroundColor: "black",
     borderRadius: 8,
     marginLeft: 8,
     marginTop: 16,
   },
-  text: {},
+  text: {
+    color: THEME.WHITE_COLOR,
+    paddingBottom: 16,
+    paddingLeft: 8,
+    paddingRight: 8,
+  },
 });
